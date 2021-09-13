08299d2f64bd5585fe8e3415d418e1a7f3dfbd55
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-09-13 16:49:59 -0500

    Chapter 13 studio completed without bonus missions.

--- SpaDay/Controllers/SpaController.cs
+++ SpaDay/Controllers/SpaController.cs
@@ -42,6 +42,10 @@ namespace SpaDay.Controllers
         [Route("/spa")]
         public IActionResult Menu(string name, string skintype, string manipedi)
         {
+            ViewBag.client = name;
+            ViewBag.skinType = skintype;
+            ViewBag.maniPedi = manipedi;
+
             List<string> facials = new List<string>()
             {
                 "Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"
@@ -55,6 +59,8 @@ namespace SpaDay.Controllers
                     appropriateFacials.Add(facials[i]);
                 }
             }
+            ViewBag.clientFacials = appropriateFacials;
+
             return View();
         }
 
