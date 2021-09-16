8f1eeeab1a509b27cb75be88fdb1889e3ade1736
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-09-15 20:16:18 -0500

    Chapter 14 studio completed without bonus missions.

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/User")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.user = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.password = newUser.Password;
+            ViewBag.verify = verify;
+            if(newUser.Password == verify)
+            {
+                ViewBag.IsError = false;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.IsError = true;
+                ViewBag.error = "Your passwords do not match!";
+                return View("Add");
+            }
+        }
+    }
+}
